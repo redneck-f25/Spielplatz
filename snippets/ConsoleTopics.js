8421e4ee6de0867144c25a37b03a8bdbeda5b366
Daniel Hammerschmidt <daniel@redneck-engineering.com>
2024-03-09 18:04:34 +0100

    Create ConsoleTopics.js

---
+++ snippets/ConsoleTopics.js
@@ -0,0 +1,25 @@
+[ 'log', 'info', 'warn', 'error' ].forEach( ( methodName ) => {
+  const nativeMethod = console[ methodName ];
+  console[ methodName + 'Topic' ] = ( topic, ...args ) => {
+    let topics = sessionStorage.__debug_ConsoleTopics;
+    if ( !topics ) {
+      nativeMethod( ...args );
+      return;
+    }
+    if ( topics[ 0 ] !== ',' ) {
+      if ( topics[ topics.length - 1 ] !== ',' ) {
+        topics = sessionStorage.__debug_ConsoleTopics = ',' + topics + ',';
+      }
+      else {
+        topics = sessionStorage.__debug_ConsoleTopics = ',' + topics;
+      }
+    }
+    else if ( topics[ topics.length - 1 ] !== ',' ) {
+      topics = sessionStorage.__debug_ConsoleTopics = topics + ',';
+    }
+    if ( topics.indexOf( ',' + topic + ',' ) >= 0 ) {
+      nativeMethod( ...args );
+    }
+  }
+  console[ methodName ] = console[ methodName + 'Topic' ].bind( undefined, undefined );
+});
